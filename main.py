fc0e89647931f5817b5acf4ac2ce8b4d928a756a
jeanp94 <xjeanpierx94@gmail.com>
2023-02-16 12:30:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hola, mundo!")
+url = 'https://www.mariaalmenara.pe'
+response = requests.get(url)
+response_time = response.elapsed.total_seconds()
+print(f'Tiempo de respuesta: {response_time} segundos')
